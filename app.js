422f635b67ac32e807ae3962d5b0ffad25a25f4b
OrigamiJoji <georgesaracc@gmail.com>
2022-12-02 00:36:24 -0800

    Update route handlers: createBlogPost, getAllBlogs, getBlog

--- app.js
+++ app.js
@@ -7,10 +7,58 @@ const app = express();
 // MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
+// TOP LEVEL
+const blogs = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/blogs.json`)
+);
 // ROUTE HANDLERS
-const getAllBlogs = (req, res) => {};
-const getBlog = (req, res) => {};
-const createBlog = (req, res) => {};
+const getAllBlogs = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: blogs.length,
+        data: {
+            blogs,
+        },
+    });
+};
+const getBlog = (req, res) => {
+    const id = req.params.id * 1;
+    if (id > blogs.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'out of range',
+        });
+    }
+
+    const blog = blogs.find((e) => e.id === id);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            blog: blog,
+        },
+    });
+};
+const createBlog = (req, res) => {
+    const newBlogId = blogs[blogs.length - 1].id + 1;
+    const date = new Date().toDateString();
+    const newBlogPost = Object.assign(
+        { id: newBlogId, date },
+        req.body
+    );
+    blogs.push(newBlogPost);
+    fs.writeFile(
+        `${__dirname}/data/blogs.json`,
+        JSON.stringify(blogs),
+        (err) => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    blog: newBlogPost,
+                },
+            });
+        }
+    );
+};
 const updateBlog = (req, res) => {};
 const deleteBlog = (req, res) => {};
 // ROUTES
